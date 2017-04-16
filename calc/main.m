226de7a638ad290d8135cd7430faa0dfa259b4a4
Idris Dose <idrisdev@gmail.com>
2017-04-16 19:16:29 +1000

    makefile for linux only

--- calc/main.m
+++ calc/main.m
@@ -7,11 +7,14 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        
-        for(int i = 0; i < argv.length; i++)){
-            print("HELLO WORLD: %c",argv[i]);
-        }
+        if (argc <= 0){
+            NSLog(@"There are no equations.");
+        } else {
+            for(int i = 0; i < (sizeof argv); i++){
+                printf("CHAR: %s \n",argv[i]);
+            }
         //NSLog(@"Hello, World!");
+        }
     }
     return 0;
 }
