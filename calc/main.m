3821f4bdb49c22e7ddd4db5d75f710cb362bf837
Idris Dose <idrisdev@gmail.com>
2017-04-17 21:29:49 +1000

    Basic Equations

--- calc/main.m
+++ calc/main.m
@@ -7,6 +7,7 @@
 
 int main(int argc, const char **argv) {
     @autoreleasepool {
+
         //Grab args into a string from char array
         //Convert Args to arithmatic
         //Do some error checking
@@ -14,14 +15,14 @@ int main(int argc, const char **argv) {
         //Print Answer.
         //Run Test Case
         //Submit.
-        
-        for(int i = 0; i < (sizeof(**argv)); i++){
-            
-            NSLog(@"CHAR: %c", *argv[i]);
-            
-        }
-        
-        
+
+
+        //Looks at basic array and then dumps.
+        NSString *equation = (char *)argv[1] ? [NSString stringWithUTF8String:(char *)argv[1]] : nil;
+        equation = [equation stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+
+        NSLog(@"Equation: %@",equation);
+
         //~22.5hrs worth of code???
         //Chyea.
     }
